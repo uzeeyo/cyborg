fcbcd2fc08ce34fe0885524ffe72003a1ad2dcfc
Jordan Minott <jgminott@gmail.com>
2024-04-15 19:51:54 +0300

    Player now faces mouse position

--- Assets/Scripts/Character/Movement/RobotMovement/RobotMovement.cs
+++ Assets/Scripts/Character/Movement/RobotMovement/RobotMovement.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RobotMovement : MonoBehaviour
@@ -9,6 +7,13 @@ public class RobotMovement : MonoBehaviour
     private void Start()
     {
         model.SpeedCurrent = model.SpeedNormal;
+    }
+
+    private void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+        transform.up = direction;
     }
 
     private void FixedUpdate()
