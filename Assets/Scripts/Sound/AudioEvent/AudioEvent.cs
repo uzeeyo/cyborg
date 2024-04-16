f4802072834a4c40e6c10645111ff477dc6d2c51
Yaygun <yusufaygunnn@hotmail.com>
2024-04-16 23:52:25 +0300

    Sound play player death
    play player death sound

--- Assets/Scripts/Sound/AudioEvent/AudioEvent.cs
+++ Assets/Scripts/Sound/AudioEvent/AudioEvent.cs
@@ -2,6 +2,5 @@ using UnityEngine;
 public abstract class AudioEvent : ScriptableObject
 {
     public abstract bool TryBegin();
-    public abstract void Play();
     public abstract void Destroy();
 }
