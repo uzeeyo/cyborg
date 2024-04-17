2883c8ea144a8f30c94976eef29aad8201211759
Yaygun <yusufaygunnn@hotmail.com>
2024-04-17 10:50:10 +0300

    UI Show Energy Bar

---
+++ Assets/Scripts/UI/EnergyBar/EnergyBar.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergyBar : MonoBehaviour
+{
+    [SerializeField] private Image EnergyImage;
+    private void Awake()
+    {
+        EventHub.E_ElectricityChangedNewRate += SetEnergeBarImageFill;
+    }
+    private void SetEnergeBarImageFill(float rate)
+    {
+        EnergyImage.fillAmount = rate;
+    }
+}
