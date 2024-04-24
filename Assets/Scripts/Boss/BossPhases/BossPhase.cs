c81b67e74baf0c2dea5d66ea872cb7eefb3016d2
Yaygun <yusufaygunnn@hotmail.com>
2024-04-24 20:29:36 +0300

    Boss phase change

--- Assets/Scripts/Boss/BossPhases/BossPhase.cs
+++ Assets/Scripts/Boss/BossPhases/BossPhase.cs
@@ -1,14 +1,22 @@
 using UnityEngine;
+using System;
 
 public abstract class BossPhase : ScriptableObject
 {
+    protected Action PhaseEndEvent;
     protected BossPhaseManager bossPhaseManager;
     public virtual void Begin(BossPhaseManager bossPhaseManager) 
     { 
         this.bossPhaseManager = bossPhaseManager;
     }
-    public abstract void Tick();
-    public abstract void MustEnd();
+    public virtual void Begin(BossPhaseManager bossPhaseManager, Action phaseEndEvent)
+    {
+        Begin(bossPhaseManager);
+        PhaseEndEvent = phaseEndEvent;
+    }
+    public virtual void Tick() { }
+    public virtual void AttackEnded() { }
+    public virtual void MustEnd() { }
     public virtual void End()
     {
         bossPhaseManager.PhaseEnded();
