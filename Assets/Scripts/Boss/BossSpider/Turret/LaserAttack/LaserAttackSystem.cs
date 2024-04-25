ae5dca1b3370eb7c2929ff555b87899ba5794d8a
Yaygun <yusufaygunnn@hotmail.com>
2024-04-25 03:25:06 +0300

    Laser open close

--- Assets/Scripts/Boss/BossSpider/Turret/LaserAttack/LaserAttackSystem.cs
+++ Assets/Scripts/Boss/BossSpider/Turret/LaserAttack/LaserAttackSystem.cs
@@ -6,7 +6,17 @@ using System;
 public class LaserAttackSystem : MonoBehaviour
 {
     public Action AttackEnded;
-    private float AnimWaitTime = 1f;
+    private float AnimWaitTime = 1.2f;
+    private float ActionTime = 1.5f;
+    [SerializeField] private GameObject Laser;
+    private Animator animator;
+    private SpiderTurret spiderTurret;
+
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        spiderTurret = GetComponent<SpiderTurret>();
+    }
     public void Begin()
     {
         StartCoroutine(Operations());
@@ -14,10 +24,26 @@ public class LaserAttackSystem : MonoBehaviour
 
     IEnumerator Operations()
     {
-        //start anim,
+        animator.SetTrigger("Laser");
         yield return new WaitForSeconds(AnimWaitTime);
-        //StartShooting
-        //End anim
+        Laser.SetActive(true);
+
+        float timer = 0;
+        while (true)
+        {
+            timer += Time.deltaTime;
+            if(timer > ActionTime)
+            {
+                spiderTurret.LaserRate(0.5f);
+                break;
+            }
+            float rate = timer / ActionTime;
+            spiderTurret.LaserRate(rate - 0.5f);
+            yield return null;
+        }
+ 
+        animator.SetTrigger("End");
+        Laser.SetActive(false);
         yield return new WaitForSeconds(AnimWaitTime);
         End();
     }
