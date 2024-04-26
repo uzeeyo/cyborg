7458d7911334faa24c9a0b5f280b2d387c267224
Yaygun <yusufaygunnn@hotmail.com>
2024-04-26 03:30:09 +0300

    Explosion Effect

---
+++ Assets/Scripts/Explosion/ExplodeEffect.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExlplodeEffect : MonoBehaviour
+{
+    float rate = 1;
+    public void SetRadius(float radius)
+    {
+        transform.localScale = new Vector3(radius, radius, radius) * rate;
+    }
+    void Start()
+    {
+        StartCoroutine(Disapear());
+    }
+    IEnumerator Disapear()
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+    }
+}   
